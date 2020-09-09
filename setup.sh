4ba488669b82ffba7bbb89082bbe70d0ff30854a
yohannTum <yohann.tumpling@gmail.com>
2020-09-09 11:10:13 +0300

    init

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 git config --global user.email "yohann.tumpling@gmail.com"
 git config --global user.name "yohannTum"
-
+firefox https://perso.liris.cnrs.fr/raphaelle.chaine/COURS/M1if02_20_21
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
